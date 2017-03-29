b500c632a8bc3c33144f4f5b404f4349ed0e6516
Agata <agata@net100ip3.intranet.piekary.net>
2017-03-29 21:12:53 +0200

    location test

--- magic-hours/ViewController.swift
+++ magic-hours/ViewController.swift
@@ -6,19 +6,53 @@
 //  Copyright © 2017 Agata. All rights reserved.
 //
 
-import UIKit
+import UIKit;
+import CoreLocation;
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+	
+	//MARK: Properties
+	let locationManager = CLLocationManager();
+	
+	//MARK: Actions
 
+	@IBAction func getLocation(_ sender: UIButton)
+	{
+		locationManager.delegate = self;
+		locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+		locationManager.requestWhenInUseAuthorization();
+		locationManager.startUpdatingLocation();
+		locationManager.requestLocation();
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		print("Got user location");
+		
+		let userLocation:CLLocation = locations[0];
+		let long = userLocation.coordinate.longitude;
+		let lat = userLocation.coordinate.latitude;
+
+		print("locations = \(lat), \(long)")
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didFailWithError error: NSError)
+	{
+		print("location error");
+	}
+	
+
 
 
 }
